8fad4704b1601cc4b78df4abb6dc14b060de9499
gaojin <1019855680@qq.com>
2018-02-04 15:12:24 +0800

    fix path,_dirname not defined 使用after_post_render代替before_post_render,

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var path = require("path");
 var fs = require('fs');
-hexo.extend.filter.register('before_post_render', function(data){
-    var file_content = fs.readFileSync(path.join(_dirname,"./push.js"),"utf-8");
+hexo.extend.filter.register('after_post_render', function(data){
+    var file_content = fs.readFileSync(path.join(__dirname,"./push.js"),"utf-8");
         data.content += file_content;
     return data;
 });
